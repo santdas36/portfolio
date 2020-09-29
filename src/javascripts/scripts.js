f9371ee3cafc1ba0e1237b6d659b75f2436b5abe
santdas36 <santdas36@gmail.com>
2020-09-29 15:09:11 +0530

    irr

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,6 +1,6 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { SVGInjector } from '@tanem/svg-injector';
+import { SVGInjector } from '@tanem/svg-injector';
 import MagnetMouse from './magnetMouse';
 
 SVGInjector(document.getElementsByClassName('inject-me'));
