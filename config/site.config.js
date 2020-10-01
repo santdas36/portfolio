abcbbc1ced3b376010a4b8d52d777f44bc52ad93
santdas36 <santdas36@gmail.com>
2020-10-01 10:28:40 +0530

    gtag

--- config/site.config.js
+++ config/site.config.js
@@ -18,7 +18,7 @@ const config = {
   site_url: 'https://dashsantosh.ga',
 
   // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
+  googleAnalyticsUA: 'UA-164978597-1',
 
   // The viewport meta tag added to your HTML page's <head> tag
   viewport: 'width=device-width,initial-scale=1',
--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -113,7 +113,15 @@ const webpackBar = new WebpackBar({
 });
 
 // Google analytics
-const CODE = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','{{ID}}','auto');ga('send','pageview');</script>`;
+const CODE = `
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ID}}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '{{ID}}');
+</script>
+`;
 
 class GoogleAnalyticsPlugin {
   constructor({ id }) {
