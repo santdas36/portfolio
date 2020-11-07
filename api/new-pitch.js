d272abfcbe2416acb3391800e1ce2441596d6f21
santdas36 <santdas36@gmail.com>
2020-11-08 00:50:36 +0530

    clg

--- api/new-pitch.js
+++ api/new-pitch.js
@@ -4,7 +4,7 @@ export default async (request, response) => {
 	if (request.method !== 'POST') {
 		return response.status(400).send('400 Bad Request');
 	}
-
+	console.log(request.body);
 	const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 	await doc.useServiceAccountAuth({
 		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -16,7 +16,7 @@ export default async (request, response) => {
 	console.log(sheet.rowCount);
 	const newRow = await sheet.addRow({
 		timestamp: new Date().toLocaleString('en-IN', {dateStyle: 'full', timeStyle: 'long'}),
-		email: request.body.get('email')
+		email: 'ds@o.do'
 	}); 
 	console.log(newRow);
 	return response.status(201).send('OK');
