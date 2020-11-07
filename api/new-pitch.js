cb6156ba05aa274e4ead306cbd0ee97567f40914
santdas36 <santdas36@gmail.com>
2020-11-08 00:47:55 +0530

    '\n'

--- api/new-pitch.js
+++ api/new-pitch.js
@@ -8,7 +8,7 @@ export default async (request, response) => {
 	const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 	await doc.useServiceAccountAuth({
 		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, \n),
+		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	});
 	await doc.loadInfo();
 	const sheet = doc.sheetsByIndex[0];
