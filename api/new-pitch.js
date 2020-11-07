2b8d97ccf777348b1855910f07823dc0dc778e13
santdas36 <santdas36@gmail.com>
2020-11-07 23:50:13 +0530

    api v1

---
+++ api/new-pitch.js
@@ -0,0 +1,23 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+export default async (request, response) => {
+	if (request.method !== 'POST') {
+		return response.status(400).send('400 Bad Request');
+	}
+
+	const doc = new GoogleSpreadsheet('1Q1TO6-jiV9_5j5UIrZDjkxBih1nAkdQDtm_LBrXx2N8');
+	await doc.useServiceAccountAuth({
+		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+		private_key: process.env.GOOGLE_PRIVATE_KEY,
+	});
+	await doc.loadInfo();
+	const sheet = doc.sheetsByIndex[0];
+	console.log(sheet.title);
+	console.log(sheet.rowCount);
+	const newRow = await sheet.addRow({
+		timestamp: new Date().toLocaleString('en-IN', {dateStyle: 'full', timeStyle: 'long'}),
+		email: request.body.get('email')
+	}); 
+	console.log(newRow);
+	return response.status(201).send('OK');
+}
