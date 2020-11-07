937e2a06fa1a2c39b187fe9361c2da9076e7af7b
santdas36 <santdas36@gmail.com>
2020-11-08 01:12:39 +0530

    localeMatcher

--- api/new-pitch.js
+++ api/new-pitch.js
@@ -15,7 +15,7 @@ export default async (request, response) => {
 	console.log(sheet.title);
 	console.log(sheet.rowCount);
 	const newRow = await sheet.addRow({
-		timestamp: new Date().toLocaleString('en-IN', {dateStyle: 'full', timeStyle: 'long'}),
+		timestamp: new Date().toLocaleString('en-IN', {dateStyle: 'full', timeStyle: 'long', localeMatcher: 'lookup'}),
 		email: 'ds@o.do'
 	}); 
 	console.log(newRow);
