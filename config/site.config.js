6b4ce09962de5fd8233e510c1227a608d55d35cf
santdas36 <santdas36@gmail.com>
2020-11-09 13:09:42 +0530

    desc

--- config/site.config.js
+++ config/site.config.js
@@ -9,16 +9,13 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'DASH SANTOSH | Personal Portfolio | Front-End Developer, UI/UX Designer',
+  site_name: 'DASH SANTOSH | Front-End Developer, UI/UX Designer | Personal Portfolio',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'defn. a 19yr old male who once happened to click on view source of a webpage unintentionally, and now is a Front-End Developer and UI/UX Designer',
+  site_description: 'Helping new businessess Expand, and the old ones Adapt to the ever-changing World of Internet, with Modern Web Solutions.',
 
   // Your website's URL, used for sitemap
   site_url: 'https://dashsantosh.ga',
-
-  // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: 'UA-164978597-1',
 
   // The viewport meta tag added to your HTML page's <head> tag
   viewport: 'width=device-width,initial-scale=1',
