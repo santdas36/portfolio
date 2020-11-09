d0b2dc320e6c2000e6167d24a45ef0763c79d34b
santdas36 <santdas36@gmail.com>
2020-11-09 15:08:44 +0530

    inline preload script

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -333,9 +333,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
-
-document.addEventListener('load', () => {
-  const preloader = document.querySelector('.preloader');
-  preloader.classList.add('loaded');
-  setTimeout(() => preloader.remove(), 2000);
-});
