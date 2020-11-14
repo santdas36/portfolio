51be4417dd0d628854c113ba93194171432585c8
santdas36 <santdas36@gmail.com>
2020-11-14 20:09:10 +0530

    arrow

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -19,7 +19,7 @@ if ('serviceWorker' in navigator) {
 document.addEventListener('DOMContentLoaded', () => {
   const cookieToast = document.querySelector('.toast.cookie');
   const closeCookie = document.querySelector('#closeCookie');
-  closeCookie.addEventListener('click', {
+  closeCookie.addEventListener('click', () => {
     localStorage.setItem('cookieConsent', true);
     cookieToast.classList.remove('shown');
   });
